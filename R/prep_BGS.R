c4b4c883ad15a2028c78eadb12d4c8172b051b0c
Malcolm Morgan <mem48@users.noreply.github.com>
2022-02-02 16:49:11 +0000

    fix bgs data crs

--- R/prep_BGS.R
+++ R/prep_BGS.R
@@ -5,13 +5,13 @@ library(piggyback)
 
 
 dir.create("tmp")
-unzip("D:/OneDrive - University of Leeds/Data/British Geological Survey/digmap625_bedrock_arc.zip",
+unzip("E:/Users/earmmor/OneDrive - University of Leeds/Data/British Geological Survey/digmap625_bedrock_arc.zip",
       exdir = "tmp")
 bedrock <- st_read("tmp/625k_V5_BEDROCK_Geology_Polygons.shp")
 unlink("tmp", recursive = TRUE)
 
 dir.create("tmp")
-unzip("D:/OneDrive - University of Leeds/Data/British Geological Survey/digmap625_superficial_arc.zip",
+unzip("E:/Users/earmmor/OneDrive - University of Leeds/Data/British Geological Survey/digmap625_superficial_arc.zip",
       exdir = "tmp")
 superficial <- st_read("tmp/UK_625k_SUPERFICIAL_Geology_Polygons.shp")
 unlink("tmp", recursive = TRUE)
