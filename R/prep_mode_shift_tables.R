90a3803f75856423b977d17875406b53410fd4a5
Malcolm Morgan <mem48@users.noreply.github.com>
2022-02-16 16:04:31 +0000

    fix typo

--- R/prep_mode_shift_tables.R
+++ R/prep_mode_shift_tables.R
@@ -1,6 +1,8 @@
 # prep the mode shift table for the package
+library(readxl)
 
-dir = "C:/Users/malco/University of Leeds/TEAM - Shared Digital Carbon Architecture - General"
+#dir = "C:/Users/malco/University of Leeds/TEAM - Shared Digital Carbon Architecture - General"
+dir = "D:/University of Leeds/TEAM - Shared Digital Carbon Architecture - Documents/General"
 
 path = file.path(dir,"WP4 – In use emissions of transport infrastructure and induced demand/Mode Shift/mode shift assumptions.xlsx")
 
@@ -18,7 +20,7 @@ modeshift$mode[modeshift$mode == "Heavy Rail"] = "Rail"
 
 modeshift$intervention_class[modeshift$intervention_class == "New"] = "New construction"
 
-modeshift$travel_mode[modeshift$travel_mode == "car driver"] = "driver"
+modeshift$travel_mode[modeshift$travel_mode == "car driver"] = "drive"
 modeshift$travel_mode[modeshift$travel_mode == "car passenger"] = "passenger"
 
 # Replace NA with 0
@@ -32,6 +34,15 @@ modeshift[4:9] <- lapply(modeshift[4:9], function(x){
   x = ifelse(x > 1, x/100, x)
   x
 })
+
+modeshift$modeshift_average <- ifelse(modeshift$modeshift_average == 0,
+                                      (modeshift$modeshift_low +modeshift$modeshift_high)/2,
+                                      modeshift$modeshift_average)
+
+modeshift$induceddemand_average <- ifelse(modeshift$induceddemand_average == 0,
+                                      (modeshift$induceddemand_low +modeshift$induceddemand_high)/2,
+                                      modeshift$induceddemand_average)
+
 
 write.csv(modeshift, "../sdca-data/package_files/mode_shifts.csv", 
           row.names = FALSE, fileEncoding = "UTF-8")
