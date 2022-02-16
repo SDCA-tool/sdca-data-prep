84d0815ba6b828e60ba0713d37f644dbabd53bb1
Malcolm Morgan <mem48@users.noreply.github.com>
2022-02-16 16:20:25 +0000

    update desire lines

--- R/prep_desire_lines.R
+++ R/prep_desire_lines.R
@@ -3,6 +3,11 @@ library(zip)
 library(piggyback)
 
 dl <- read_sf("../../ITSleeds/NTEM2OD/data/NTEM/NTEM_desire_lines.geojson")
+#2090770 to 2083070
+dl <- dl[dl$from != dl$to,]
+
+st_precision(dl) <- 10000
+
 write_sf(dl,"data/desire_lines.geojson")
 
 zip::zip("data/desire_lines.geojson.zip", 
